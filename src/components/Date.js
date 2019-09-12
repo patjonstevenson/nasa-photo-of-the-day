2f9b3f753368e97f6f23bc69d7a0c98bd32554b6
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-09-12 15:06:31 -0700

    made PhotoCard a reactstrap Card, added react-youtube for possibility of youtube video instead of image, changed the way I test for video vs. image, added a solution for extracting video ID that I found on stackoverflow, changed Date component to a reactstrap Form

--- src/components/Date.js
+++ src/components/Date.js
@@ -1,10 +1,34 @@
 import React, { useState } from "react";
 import moment from "moment";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 export default function Date({ updater }) {
   const [newDate, setNewDate] = useState("");
   return (
-    <div>
+    <Form
+      onSubmit={event => {
+        event.preventDefault();
+        updater(newDate);
+      }}
+    >
+      <FormGroup>
+        <Label for="exampleDate">Date </Label>
+        <Input
+          type="date"
+          name="date"
+          id="exampleDate"
+          placeholder="date placeholder"
+          value={newDate}
+          onChange={event => {
+            event.preventDefault();
+            setNewDate(event.target.value);
+          }}
+        />
+      </FormGroup>
+      <Button type="submit">Submit</Button>
+    </Form>
+
+    /*<div>
       <form
         onSubmit={event => {
           event.preventDefault();
@@ -28,5 +52,6 @@ export default function Date({ updater }) {
         <button type="submit">Submit</button>
       </form>
     </div>
+          */
   );
 }
--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,22 +1,44 @@
 import React from "react";
+import {
+  Card,
+  CardTitle,
+  CardText,
+  CardBody,
+  CardSubtitle,
+  CardImg
+} from "reactstrap";
+import YouTube from "react-youtube";
 
 export default function PhotoCard({ imgUrl, hdUrl, date, explanation, title }) {
-  const media =
-    String(imgUrl).substr(-3, 3) === "jpg" ||
-    String(imgUrl).substr(-4, 4) === "jpeg" ? (
-      <a href={hdUrl}>
-        <img className="photo" src={imgUrl} alt="NASA photo" />
-      </a>
-    ) : (
-      <iframe width="420" height="315" src={imgUrl}></iframe>
-    );
+  console.log(
+    String(imgUrl)
+      .split("?")[0]
+      .split("/")[-1]
+  );
+  const med = String(imgUrl)
+    .split(".")
+    .includes("youtube") ? (
+    <YouTube
+      /* Solution to get videoId from url found 
+      at https://stackoverflow.com/questions/43891036/extract-video-id-from-youtube-url  */
+      videoId={String(imgUrl)
+        .split("?")[0]
+        .split("/")
+        .pop()}
+      opts={{ width: "100%" }}
+    />
+  ) : (
+    <CardImg top width="100%" src={imgUrl} alt={title}></CardImg>
+  );
 
   return (
-    <div className="photo-card">
-      {media}
-      <h2>{title}</h2>
-      <h3>{date}</h3>
-      <p>{explanation}</p>
-    </div>
+    <Card className="photo-card">
+      <CardBody>
+        {med}
+        <CardTitle>{title}</CardTitle>
+        <CardSubtitle>{date}</CardSubtitle>
+        <CardText>{explanation}</CardText>
+      </CardBody>
+    </Card>
   );
 }
